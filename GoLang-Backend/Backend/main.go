3a3435da3f78d8aafe9d09fa4c485346d7fa8f5d
MR-VL <cosmmo6@gmail.com>
2025-08-07 15:42:27 -0500

    Create allowed extension list

--- GoLang-Backend/Backend/main.go
+++ GoLang-Backend/Backend/main.go
@@ -1,21 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
-
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+var allowedExtensions = map[string]bool{
+	".7z":   true,
+	".gif":  true,
+	".gz":   true,
+	".jpeg": true,
+	".jpg":  true,
+	".m4a":  true,
+	".mov":  true,
+	".mp3":  true,
+	".mp4":  true,
+	".pdf":  true,
+	".png":  true,
+	".rar":  true,
+	".tar":  true,
+	".txt":  true,
+	".wav":  true,
+	".zip":  true,
+}
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
-	}
 }
